abd867fd5c7f9328374c57d4896bfe3c08a3e68e
codergb <betweent@126.com>
2024-04-17 21:32:28 +0800

    分片进度

--- client/src/components/File.vue
+++ client/src/components/File.vue
@@ -1,31 +1,33 @@
 <template>
   <div>
     <div class="header-controller">
-      <input type="file" @change="handleFileUpload">文件上传</input>
+      <div>上传进度{{ progress }}%</div>
+      <input type="file" @change="handleFileUpload" />
     </div>
   </div>
 </template>
 <script lang="jsx" setup name="File">
-import { getFileHash, sliceFile,checkFileMd5 } from "@/utils/shared";
-import { CHUNK_SIZE } from "@/constant"
+import { getFileHash, sliceFile, checkFileMd5 } from "@/utils/shared";
+import { CHUNK_SIZE } from "@/constant";
 import axios from "axios";
 import { ref, reactive } from "vue";
+const progress = ref(0);
+
+let count = 0;
 const handleFileUpload = async (e) => {
-  const file = e.target.files[0]
+  const file = e.target.files[0];
   const ret = await getFileHash(file);
   const md5Value = ret.HASH;
- 
-
   const fileChunks = sliceFile(file);
-
   const { isUpload, chunkStatusList } = await checkFileMd5(md5Value);
-  console.log(isUpload);
   if (!isUpload) {
-    const hasEmptyChunk = chunkStatusList.findIndex((item) => item === 0);
-    if (hasEmptyChunk === -1) {
+    const hasEmptyChunk = chunkStatusList.filter((item) => item === 0);
+    if (hasEmptyChunk.length === 0) {
       console.log("上传成功");
+      progress.value = 100;
       return;
     } else {
+      count = fileChunks.length - hasEmptyChunk.length;
       chunkStatusList.forEach((item, index) => {
         if (chunkStatusList[index] !== 1) {
           let formData = new FormData();
@@ -33,46 +35,47 @@ const handleFileUpload = async (e) => {
           formData.append("chunkSize", CHUNK_SIZE);
           formData.append("chunkIndex", index);
           formData.append("fileHash", md5Value);
-          formData.append("file", new File([fileChunks[index]], fileChunks[index].fileName))  
+          formData.append(
+            "file",
+            new File([fileChunks[index]], fileChunks[index].fileName)
+          );
           axios({
-    url: "/api/file/upload",
-    method:'post',
-    data: formData,
-    headers: {
-      'Content-type': 'multipart/form-data'
-    }
-  }).then((res) => {
-    console.log(res.data);
-  })  
+            url: "/api/file/upload",
+            method: "post",
+            data: formData,
+            headers: {
+              "Content-type": "multipart/form-data",
+            },
+          }).then((res) => {
+            count += 1;
+            progress.value = count / fileChunks.length * 100;
+          });
         }
-      })
+      });
     }
   } else {
-    for (let i = 0; i < fileChunks.length; i++){
-    const formData = new FormData();
-    let item = fileChunks[i];
-    formData.append("totalNumber", fileChunks.length);
-    formData.append("chunkSize", CHUNK_SIZE);
-    formData.append("chunkIndex", i);
-    formData.append("fileHash", md5Value);
-    formData.append("file", new File([item.fileChunk], item.fileName))  
-    axios({
-    url: "/api/file/upload",
-    method:'post',
-    data: formData,
-    headers: {
-      'Content-type': 'multipart/form-data'
+    for (let i = 0; i < fileChunks.length; i++) {
+      const formData = new FormData();
+      let item = fileChunks[i];
+      formData.append("totalNumber", fileChunks.length);
+      formData.append("chunkSize", CHUNK_SIZE);
+      formData.append("chunkIndex", i);
+      formData.append("fileHash", md5Value);
+      formData.append("file", new File([item.fileChunk], item.fileName));
+      axios({
+        url: "/api/file/upload",
+        method: "post",
+        data: formData,
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      }).then((res) => {
+        
+        count += 1;
+        progress.value = count / fileChunks.length * 100;
+      });
     }
-  }).then((res) => {
-    console.log(res.data);
-  })  
-   
   }
-  }
-  return 
-  
-  
-  
-}
+  return;
+};
 </script>
-
